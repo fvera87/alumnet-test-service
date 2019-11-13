2dbec06f9d651687cfc7cac587da5b5b032ea7cd
Fernando Vera Lopez <fernando.vl87@gmail.com>
2019-11-13 11:50:30 +0100

    remove useless files

--- src/helpers/isValidContentType.ts
+++
@@ -1,9 +0,0 @@
-import { CONTENT_TYPE_KEY, CONTENT_TYPE_VALUE } from '../config/constants';
-
-export const isValidContentType = (headers: object) => {
-  const contentType = headers[CONTENT_TYPE_KEY] || headers[CONTENT_TYPE_KEY.toLocaleLowerCase()];
-  if (!contentType || contentType.toLocaleLowerCase() !== CONTENT_TYPE_VALUE.toLocaleLowerCase()) {
-    return false;
-  }
-  return true;
-};
